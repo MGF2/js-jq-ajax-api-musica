33d18c21c5c0276e9b9e15b4547ece326d6da1a7
Unknown <fioremartina@hotmail.com>
2020-08-28 16:54:42 +0200

    no options

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 $(document).ready(function() {
-	//Code
+
+  $.ajax(
+    {
+      'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+      'method': 'GET',
+      'success': function(risposta){
+
+        // loop to get objects
+        for ( var i = 0 ; i < risposta.response.length; i++) {
+
+          console.log(risposta);
+
+          var source = $('#entry-template').html();
+  			  var template = Handlebars.compile(source);
+
+          //object content
+          var content = risposta.response[i];
+  				var html = template(content);
+
+          //inserimento object in html
+  			  $('.cds-container').append(html);
+
+        }
+
+      },
+      'error':function(){
+        alert('errore!');
+      }
+    }
+  );
+
 });
+
+// FUNCTIONS
+
+// leggiDati(risposta); nope
+
+// function leggiDati (d){
+// 	var source = $('#entry-template').html();
+// 	var template = Handlebars.compile(source);
+//
+// 	var html = template(d);
+// 	$('.container').append(html);
+// }
